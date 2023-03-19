a8c7669ff30b489902036bb2eff027774e04d0f1
Jose Annunziato <jose.annunziato@sonos.com>
2023-03-19 08:50:26 -0400

    try again

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const server = express()
+const express = require("express");
+const server = express();
 const port = process.env.PORT || 4000;
-server.get('/', (req, res) => res.send('<h1>Welcome to Web Dev</h1>'))
-server.get('/hello', (req, res) => res.send('Hello World'))
-server.listen(port)
+server.get("/", (req, res) => res.send("<h1>Welcome to Web Dev</h1>"));
+server.get("/hello", (req, res) => res.send("Hello World try this again"));
+server.listen(port);
