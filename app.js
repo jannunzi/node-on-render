950148ba683e2cf346d34764e4539751dde56155
Jose Annunziato <jose.annunziato@sonos.com>
2023-03-19 08:48:45 -0400

    switched to main

---
+++ app.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 4000;
+app.get("/", (req, res) => res.send("<h1>Welcome to Web Dev</h1>"));
+app.get("/hello", (req, res) => res.send("Hello World 123 on main"));
+app.listen(port);
